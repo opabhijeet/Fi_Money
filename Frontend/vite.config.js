56f85819c3a7f1be8d6f7ebd82360015ce396751
opabhijeet <abhijeet.2201164ec@iiitbh.ac.in>
2025-07-26 03:48:09 +0530

    tailwind config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,10 +9,13 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:8080/api',
         changeOrigin: true,
         secure: false,
       }
-    }
+    },
+    plugins: [
+      tailwindcss(),
+    ],
   }
 })
